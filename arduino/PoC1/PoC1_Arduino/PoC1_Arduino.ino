fb78eb2d290c4ede6b67cc3b95fdc31150a63e7c
Henry Wu <henrywuu1122@gmail.com>
2023-11-30 18:24:55 -0500

    implemented py command line prompt ctrl

--- arduino/PoC1/PoC1_Arduino/PoC1_Arduino.ino
+++ arduino/PoC1/PoC1_Arduino/PoC1_Arduino.ino
@@ -1,4 +1,5 @@
 #include <PacketSerial.h>
+#include <SoftwareSerial.h>
 #include "PressureSensor.h"
 #include "CytronController.h"
 #include "PIDController.h"
@@ -19,58 +20,169 @@ enum Commands {
   SET_MOTOR_SPEED = 1,
   SET_PID_VALUES = 2,
   SET_PRESSURE = 3,
-  STOP = 4
+  STOP = 4,
+
+  EXTRACTION_STOPPED = 8,
+  PRESSURE_READING = 9
 };
 
+SoftwareSerial debug(11, 10);  // RX, TX
 PacketSerial pSerial;
+
 
 PressureSensor pSensor(pressurePin);
 CytronController motor(pwmPin, dirPin);
 PIDController pidController(motor, pSensor);
 
+// PID Extraction Loop Parameters
+bool isPIDRunning = false;
+unsigned long pidStartTime = 0;
+const unsigned long pidRunDuration = 4000; // PID Loop timeout
+
+//
 float pressure;
-uint8_t outBuffer[4];  // floats are 4-bytes each in Ardiuno
-const float confirm_float = 999.999;
+uint8_t outBuffer[sizeof(short) + sizeof(float)];
 
 void setup() {
-  pidController.setParameters(PRESSURE_SETPOINT, kP, kI, kD);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  motor.stop();
 
-  pSerial.begin(115200);  // Start the serial communication
-  // Serial.begin(9600);
+  pSerial.begin(115200);
+
+  debug.begin(4800);
+  debug.println("debug print test...");
+
   pSerial.setPacketHandler(&onPacketReceived);
 }
 
 void loop() {
-  // pressure = pSensor.readPressure();
   pSerial.update();
-  // pidController.update();
+  pressure = pSensor.readPressure();
 
-  // memcpy(outBuffer, &pressure, sizeof(pressure));
-  // pSerial.send(outBuffer, sizeof(outBuffer));
-  delay(1);
+  if (isPIDRunning) {
+    // Check if 5 seconds have passed or a stop command was received
+    if (millis() - pidStartTime > pidRunDuration) {
+      isPIDRunning = false;
+      motor.stop();  // Stop the motor after 5 seconds
+      sendExtractionStopped();
+      debug.println("Extraction duration reached");
+    } else {
+      // BUFFER SEEMS TO OVER FLOW IF I PRINT THIS AS WELL. IF delay 30 ms it works
+      // Can't be doing this in actual. 
+      // debug.println("extracting...");
+      pidController.update();
+      sendPressureReading();
+    }
+  }
+
+  delay(20);
 }
 
 void onPacketReceived(const uint8_t* buffer, size_t size) {
-  if (size == sizeof(short) + sizeof(float)) {
-    // // Create variables to hold the extracted data
-    short command;
-    float value;
+  // Ensure the buffer has at least the size of a short (for the command)
+  if (size < sizeof(short)) {
+    debug.print("Not enough data to read a command. Size: ");
+    debug.println(size);
+    debug.print("Data: ");
 
-    // // Extract the integer (first 4 bytes)
-    memcpy(&command, buffer, sizeof(short));
-    memcpy(&value, buffer + sizeof(short), sizeof(float));
-
-    pSerial.send(reinterpret_cast<uint8_t*>(&value), sizeof(float));
-
-    if (value == 4.7) {
-      for (size_t i = 0; i < 2; ++i) {
-        digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED on
-        delay(200);                       // Wait for 500 milliseconds
-        digitalWrite(LED_BUILTIN, LOW);   // Turn the LED off
-        delay(200);                       // Wait for 500 milliseconds
+    if (size > 0) {
+          // Print the single byte in hexadecimal format
+          debug.print("0x");
+          debug.println(buffer[0], HEX);  // Print the first byte in HEX
       }
-    }
+    return;
   }
+
+  // Extract the command
+  short command;
+  memcpy(&command, buffer, sizeof(short));
+  debug.print("Received Command: ");
+  debug.println(command);
+
+  // Switch on the command
+  switch (command) {
+    case STOP:
+      {
+        motor.stop();
+        isPIDRunning = false; // Stop PID control
+        sendExtractionStopped();
+        debug.println("Stopped");
+        break;
+      }
+    case SET_MOTOR_SPEED:
+      {
+        // Check if the size is correct (command + 1 float)
+        if (size == sizeof(short) + sizeof(float)) {
+          float speed;
+          memcpy(&speed, buffer + sizeof(short), sizeof(float));
+          motor.setSpeed(speed);  // Assuming a method to set motor speed
+          debug.println("Motor speed set:");
+          debug.print("Speed: ");
+          debug.println(speed);
+        } else {
+          debug.println("Invalid Set Motor Speed Command");
+        }
+        break;
+      }
+
+    case SET_PID_VALUES:
+      {
+        // Check if the size is correct (command + 4 floats)
+        if (size == sizeof(short) + 4 * sizeof(float)) {
+          // Create variables to hold the PID values
+          float p, i, d, setpoint;
+
+          // Extract the PID values
+          memcpy(&p, buffer + sizeof(short), sizeof(float));
+          memcpy(&i, buffer + sizeof(short) + sizeof(float), sizeof(float));
+          memcpy(&d, buffer + sizeof(short) + 2 * sizeof(float), sizeof(float));
+          memcpy(&setpoint, buffer + sizeof(short) + 3 * sizeof(float), sizeof(float));
+
+
+          pidController.setParameters(setpoint, p, i, d);
+          {  // Debug Prints
+            debug.println("PID values set:");
+            debug.print("Setpoint: ");
+            debug.println(setpoint);
+            debug.print("P: ");
+            debug.println(p);
+            debug.print("I: ");
+            debug.println(i);
+            debug.print("D: ");
+            debug.println(d);
+          }
+          
+          // Start PID control
+          debug.println("extracting & sending pressure...");
+          isPIDRunning = true;
+          pidStartTime = millis();  // Record the start time
+          break;
+
+        } else {
+          debug.println("Invalid PID Command");
+        }
+        break;
+      }
+      // Handle other cases here
+      // ...
+  }
+  debug.println();
+}
+
+void sendExtractionStopped() {
+    short command = EXTRACTION_STOPPED;
+    uint8_t buffer[sizeof(short)];
+    memcpy(buffer, &command, sizeof(command));
+    pSerial.send(buffer, sizeof(buffer));
+    debug.print("Sending Extraction Stopped. Sent bytes: ");
+    debug.println(sizeof(buffer));
+}
+
+void sendPressureReading() {
+    short command = PRESSURE_READING;
+    uint8_t buffer[sizeof(short) + sizeof(float)];
+    memcpy(buffer, &command, sizeof(command));
+    memcpy(buffer + sizeof(command), &pressure, sizeof(pressure));
+    pSerial.send(buffer, sizeof(buffer));
+    // debug.print("Sending Pressure. Sent bytes: ");
+    // debug.println(sizeof(buffer));
 }
--- py/serial_comms.py
+++ py/serial_comms.py
@@ -17,46 +17,191 @@ arduino_port = None
 
 zeroByte = b'\x00' # COBS 1-byte delimiter is hex zero as a (binary) bytes character
 
-class Order(Enum):
-    """
-    Pre-defined orders
-    """
-    HELLO = 0
-    MOTOR = 1
-    SPEED = 2
-    PRESSURE_SETPOINT = 3
-    PID_P = 4
-    PID_I = 5
-    PID_D = 6
-    STOP = 7
+class Command(Enum):
+    """ Enum for command types """
+    SET_MOTOR_SPEED = 1
+    SET_PID_VALUES = 2
+    SET_PRESSURE = 3
+    STOP = 4
 
-    FEEDBACK_PRESSURE = 8
-    FEEDBACK_PWM = 9
-    FEEDBACK_SPEED = 10
+    EXTRACTION_STOPPED = 8
+    PRESSURE_READING = 9
 
+# Connect to Arduino Serial
 try:
-    for p in ports:
-        # print(p)
+    arduino_serial = serial.Serial(port="/dev/cu.usbmodem144101", baudrate=115200, timeout=None)
+    if not arduino_serial.is_open:
+        raise Exception("Failed to open serial connection to Arduino")
+    print(f"Connected to: {arduino_serial.port}")
 
-        if "Arduino" in p.description:
-            arduino_port = p.device
-            break
+    # for p in ports:
+    #     # print(p)
+
+    #     if "Arduino" in p.description:
+    #         arduino_port = p.device
+    #         break
     
-    if arduino_port:
-        arduino_serial = serial.Serial(port=arduino_port, baudrate=115200, timeout=None)
-        if not arduino_serial.is_open:
-                raise Exception("Failed to open serial connection to Arduino")
-        print(f"Connected to: {p}")
-    else:
-        # No Arduino found before loop ends
-        print(ports)
-        raise Exception("No Arduino Found. Please check connection")
+    # if arduino_port:
+    #     arduino_serial = serial.Serial(port=arduino_port, baudrate=115200, timeout=None)
+    #     if not arduino_serial.is_open:
+    #             raise Exception("Failed to open serial connection to Arduino")
+    #     print(f"Connected to: {p}")
+    # else:
+    #     # No Arduino found before loop ends
+    #     print(ports)
+    #     raise Exception("No Arduino Found. Please check connection")
 
 except Exception as e:
      print(f"Error: {e}")
 
+""" Command Packet Creators """
+def create_pid_packet(p, i, d, setpoint):
+    # SUPER IMPORTANT
+    packet = struct.pack('<hffff', Command.SET_PID_VALUES.value, p, i, d, setpoint)
+    encoded_packet = cobs.encode(packet) + b'\x00'
+    return encoded_packet
+
+def create_motor_speed_packet(speed):
+    packet = struct.pack('<hf', Command.SET_MOTOR_SPEED.value, speed)
+    encoded_packet = cobs.encode(packet) + b'\x00'
+    return encoded_packet
+
+def create_stop_packet():
+    packet = struct.pack('<h', Command.STOP.value)
+    encoded_packet = cobs.encode(packet) + b'\x00'
+    return encoded_packet
+
+def receive_command(serial_conn):
+    incoming_bytes = serial_conn.read_until(zeroByte)
+    if len(incoming_bytes) < 2:  # At least 2 bytes (for a short command)
+        print("Packet < 2")
+        return None, None
+
+    data_raw = incoming_bytes[:-1]  # Exclude the trailing zero byte
+    data_decoded = cobs.decode(data_raw)
+    
+    if len(data_decoded) >= 2:  # At least 2 bytes for command
+        command = struct.unpack('<h', data_decoded[:2])[0]
+
+        if command == Command.EXTRACTION_STOPPED.value:
+            return command, None
+        elif command == Command.PRESSURE_READING.value and len(data_decoded) == 6:  # command + float
+            pressure_value = struct.unpack('<f', data_decoded[2:])[0]
+            return command, pressure_value
+    
+    return None, None
+
+# Wait for Arduino to wake up
+time.sleep(2)
+
 arduino_serial.reset_input_buffer()
 arduino_serial.reset_output_buffer()
+
+# Example usage
+pid_packet = create_pid_packet(500, 3, 0, 4)
+motor_up_packet = create_motor_speed_packet(-100)
+motor_down_packet = create_motor_speed_packet(100)
+stop_packet = create_stop_packet()
+
+try:
+    while True:
+        # Get user input
+        text = input("Enter command (up/down/pid/stop/exit): ").strip().lower()
+
+        # Handle the user input
+        if text == 'up':
+            arduino_serial.write(motor_up_packet)
+            time.sleep(1)
+            arduino_serial.write(stop_packet)
+
+        elif text == 'down':
+            arduino_serial.write(motor_down_packet)
+            time.sleep(1)
+            arduino_serial.write(stop_packet)
+
+        elif text == 'pid':
+            p = float(input("Enter P value: "))
+            i = float(input("Enter I value: "))
+            d = float(input("Enter D value: "))
+            setpoint = float(input("Enter setpoint value: "))
+            packet = create_pid_packet(p, i, d, setpoint)
+            arduino_serial.write(packet)
+
+            # Read Pressure
+            while True:
+                command, value = receive_command(arduino_serial)
+                print("Recieved Command: {0}, Value: {1}".format(command, value))
+
+                if command is not None:
+                    if command == Command.EXTRACTION_STOPPED.value:
+                        print("Extraction Stopped.")
+                        break
+                    elif command == Command.PRESSURE_READING.value:
+                        print(f"Pressure Reading: {value}")
+
+        elif text == 'stop':
+            arduino_serial.write(stop_packet)
+
+        elif text == 'exit':
+            break
+
+        else:
+            print("Invalid command.")   
+except KeyboardInterrupt:
+    print("Keyboard Interrupted")
+    arduino_serial.write(stop_packet)
+    sys.exit(0)
+finally:
+    print("Finally")
+    arduino_serial.write(stop_packet)
+    sys.exit(0)
+
+try:
+    # Sending PID packet
+    arduino_serial.write(pid_packet)
+
+    # # Sending Motor Speed packet
+    # arduino_serial.write(motor_speed_packet)
+    # time.sleep(2)
+
+    # Loop to read Arduino pressure feedback
+    while(True):
+        ## COBS ## 
+        incoming_bytes = arduino_serial.read_until( zeroByte )
+        print(incoming_bytes)
+
+        # take everything except the trailing zero byte, b'\x00'
+        n_incoming_bytes = len(incoming_bytes)
+        data_raw = incoming_bytes[0:(n_incoming_bytes-1)]
+
+        # recover binary data encoded on Arduino
+        data_decoded = cobs.decode( data_raw ) 
+        n_data_decoded = len(data_decoded)
+
+        print(data_decoded)
+
+        if (n_data_decoded == 4):
+            # floats in python from the Arduino
+            num = struct.unpack('f',data_decoded)
+            print(num)
+
+    # Sending Stop packet
+    num_of_outgoing_bytes = arduino_serial.write(stop_packet)
+    print( "python wrote {0} bytes. command_encoded: {1}".format(num_of_outgoing_bytes, stop_packet))
+    time.sleep(2)
+
+except Exception as e:
+    print(f"Error: {e}")
+finally:
+    arduino_serial.close()
+    sys.exit()  # Exit the program
+
+
+while True:
+    pass
+
+
+## ARCHIVE ##
 
 buffer = bytearray()
 
@@ -67,8 +212,7 @@ print(len(command))
 command_encoded = cobs.encode(command) 
 print(len(command_encoded))
 
-# Wait for Arduino to wake up
-time.sleep(3)
+
 
 while True:
     num_of_outgoing_bytes = arduino_serial.write(command_encoded + b'\x00')
@@ -92,4 +236,3 @@ while True:
         # floats in python from the Arduino
         num = struct.unpack('f',data_decoded)
         print(num)
-    time.sleep(1)

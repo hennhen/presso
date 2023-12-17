b38276c5ba3d6f903f0afff742412cea0a2b9c4c
hennhen <henrywuu1122@gmail.com>
2023-12-17 06:24:07 -0500

    fixed the serial communication ```

--- arduino/PoC1/lib/RoboController/RoboController.cpp
+++ arduino/PoC1/lib/RoboController/RoboController.cpp
@@ -1,12 +1,18 @@
 #include "RoboController.h"
 
-RoboController::RoboController(HardwareSerial *roboSerial, uint32_t baudrate)
-: _roboSerial(roboSerial), _baudrate(baudrate), _roboClaw(roboSerial, 10000) {
+RoboController::RoboController(HardwareSerial *roboSerial, long baudrate)
+: _roboSerial(roboSerial), _baudrate(baudrate), _roboClaw(roboSerial, 1) {
+    // Constructor now only stores parameters
+}
+
+void RoboController::init() {
     _roboClaw.begin(_baudrate);
-    // Serial1.println("RoboClaw Initialized");
-    _roboClaw.ForwardM1(_address, 50); // Stop the motor
+    Serial1.println("RoboClaw Initialized");
+
+    // Test the motor
+    _roboClaw.ForwardM1(_address, 50);
     delay(100);
-    _roboClaw.ForwardM1(_address, 0); // Stop the motor
+    _roboClaw.ForwardM1(_address, 0);
 }
 
 void RoboController::setSpeed(short value) {
